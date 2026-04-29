e5ae73a12cf61ac4975e25a6378fbfb29385a358
Zitach <1063134044@qq.com>
2026-04-29 16:18:02 +0800

    feat: add home Riverpod providers

---
+++ frontend/lib/features/home/presentation/providers/home_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shop_demo/features/home/data/home_repository.dart';
+import 'package:shop_demo/features/home/domain/category.dart';
+import 'package:shop_demo/features/home/domain/listing_card.dart';
+
+final homeRepositoryProvider = Provider<HomeRepository>((ref) {
+  return HomeRepository();
+});
+
+final categoriesProvider = FutureProvider.autoDispose<List<Category>>((ref) {
+  return ref.watch(homeRepositoryProvider).getCategories();
+});
+
+final featuredListingsProvider = FutureProvider.autoDispose<List<ListingCard>>((ref) {
+  return ref.watch(homeRepositoryProvider).getFeaturedListings();
+});
